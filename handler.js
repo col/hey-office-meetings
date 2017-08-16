69319c779f96b3090cb76cd71790dcf47b8061eb
Col Harris <col.w.harris@gmail.com>
2017-08-16 20:59:54 +0800

    Make the fullfillment response dynamic

--- handler.js
+++ handler.js
@@ -16,7 +16,7 @@ var handlers = {
     var startTime = moment().hours(startHourMin[0]).minutes(startHourMin[1]).seconds(0).milliseconds(0)
     var endTime = moment(startTime).add(1, 'hours')
     CalendarUtils.createEvent('Booked by HeyOffice', startTime, endTime, 'charris@thoughtworks.com', (event) => {
-      this.emit(':tell', "Ok, I've booked Amoy from 8pm today for an hour");
+      this.emit(':tell', `Ok, I've booked Amoy from ${startTime.format("ha")} today for ${endTime.from(startTime, true)}`);
     })
   }
 
